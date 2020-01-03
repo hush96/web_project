7014cad793c6737580679e72aeaf68f683c91b87
shijingxue <13487110336@163.com>
2020-01-03 19:22:25 +0800

    商品列表，商品详情，购物车页面完成

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,39 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-
+import axios from 'axios'
+import List from '../components/goods/List.vue'
+import Detail from '../components/goods/Detail.vue'
+import Goodsdesc from '../components/goods/Goodsdesc.vue'
+import Cart from '../components/goods/Cart.vue'
 Vue.use(VueRouter)
+
+axios.defaults.baseURL = 'http://www.liulongbin.top:3005/'
+Vue.prototype.$http = axios
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: Home
+    redirect: '/goods/list'
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/goods/list',
+    name: 'list',
+    component: List
+  },
+  {
+    path: '/goods/detail/:id',
+    name: 'detail',
+    component: Detail
+  },
+  {
+    path: '/goodsdesc/:id',
+    name: 'goodsdesc',
+    component: Goodsdesc
+  },
+  {
+    path: '/shopcar',
+    name: 'cart',
+    component: Cart
   }
 ]
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,56 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import axios from 'axios'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    goodList: localStorage['goodList'] ? JSON.parse(localStorage['goodList']) : [],
+    num: ''
   },
   mutations: {
+    initList (state, data) {
+      state.goodList = data
+    },
+    addNum (state, obj) {
+      state.num = obj
+    },
+    add2Car (state, obj) {
+      console.log(obj)
+      if (state.goodList[obj.id] !== undefined) {
+        // 商品存在
+        state.goodList[obj.id] += obj.num
+      } else {
+        // 商品不存在
+        // state.goodList[obj.id] = obj.num
+        // 如果是动态增加的属性 必须使用Vue.set才可以跟踪数据改变
+        // 参数1:对象  参数2:属性名  参数3:属性值
+        Vue.set(state.goodList, obj.id, obj.num)
+        this.state.num = obj.num
+      }
+      console.log(state)
+    }
+
   },
   actions: {
+    // 获取页面数据
+    async getshopcarlist (context) {
+      const { data: res } = await axios.get('/api/goods/getshopcarlist/87,88,89')
+      console.log(res)
+      context.commit('initList', res.message)
+    }
   },
   modules: {
+
+  },
+  getters: {
+    money (state) {
+      let money = 0
+      state.goodList.forEach(item => {
+        money += item.sell_price * state.num
+      })
+      console.log(money)
+      return money
+    }
   }
 })
