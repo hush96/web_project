87e42ade547816ba2adbe7bbb04af2bdc0170cbd
shijingxue <13487110336@163.com>
2020-01-04 08:42:25 +0800

    实现了添加到购物车功能

--- src/components/goods/Cart.vue
+++ src/components/goods/Cart.vue
@@ -1,68 +1,94 @@
 <template>
   <div>
-     <van-pull-refresh v-model="isLoading" @refresh="onRefresh" success-text="刷新成功">
+    <van-pull-refresh v-model="isLoading" @refresh="onRefresh" success-text="刷新成功">
       <van-swipe-cell>
-    <van-card
-      :num="item.cou"
-      :price="item.sell_price"
-      :title="item.title"
-      :thumb="item.thumb_path" v-for="item in this.$store.state.goodList" :key="item.id">
-      <!-- 加减按钮 -->
-      <div slot="footer">
-        <van-stepper  :model="num" min="1" @change="onChange"/>
-      </div>
+        <van-card
+          :num="item.cou"
+          :desc="'￥' + item.sell_price"
+          :title="item.title"
+          :thumb="item.thumb_path"
+          v-for="item in goodsAll"
+          :key="item.id"
+        >
+          <!-- 加减按钮 -->
+          <div slot="footer">
+            <van-stepper :model="num" min="1" @change="onChange" />
+          </div>
 
-      <!-- 右滑删除 -->
-        <template slot="right">
-          <van-button square type="danger" text="删除" />
-        </template>
-
-    </van-card>
+          <!-- 右滑删除 -->
+          <template slot="right">
+            <van-button square type="danger" text="删除" />
+          </template>
+        </van-card>
       </van-swipe-cell>
     </van-pull-refresh>
-    <!-- <van-submit-bar :price="$store.getters.money" button-text="提交订单" /> -->
-    <van-submit-bar :price="$store.getters.money" button-text="提交订单" />
+    <van-submit-bar :price="allPrice * 100" button-text="提交订单" />
   </div>
 </template>
 
 <script>
-// import { mapGetters } from 'vuex'
 export default {
-  data () {
+  data() {
     return {
       goodList: [],
+      goodsAll: [],
       num: '',
-      allPrice: '',
-      isLoading: false
+      allPrice: 0,
+      isLoading: false,
+      urlId: ''
     }
   },
-  created () {
-    this.$store.dispatch('getshopcarlist')
+  created() {
+    this.$store.commit('get')
+    this.getId()
+    this.getList()
   },
   methods: {
     // 异步获取按钮num
-    onChange (num) {
-    },
+    onChange(num) {},
     // 下拉刷新
-    onRefresh () {
+    onRefresh() {
       setTimeout(() => {
         this.isLoading = false
       }, 300)
+    },
+    getId() {
+      this.goodList = this.$store.state.arr
+      this.goodList.forEach(item => {
+        this.urlId += ',' + item.id
+      })
+
+      this.urlId = this.urlId.substring(1, this.urlId.length)
+      // console.log(this.urlId)
+    },
+    async getList() {
+      const { data: res } = await this.$http.get(`/api/goods/getshopcarlist/${this.urlId}`)
+      if (res.status !== 0) {
+        return this.$toast('获取参数失败')
+      }
+      this.goodsAll = res.message
+      console.log(res.message)
+      // res.message.forEach(item => {
+      //   this.allPrice += item.sell_price
+      // })
     }
   },
   computed: {
-    // ...mapState(['goodList', 'num']),
-    // ...mapGetters(['money'])
   }
 }
 </script>
 
 <style lang="less" scoped>
-.van-card__price{
+.van-card__price {
   color: red;
   font-size: 15px;
 }
 .van-card__bottom {
   margin-bottom: 45px;
+}
+.van-card__desc {
+  float: left;
+  color: red;
+  padding-left: 15px;
 }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,56 +1,45 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import axios from 'axios'
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    goodList: localStorage['goodList'] ? JSON.parse(localStorage['goodList']) : [],
-    num: ''
+    arr: [],
+    count: 0
   },
   mutations: {
-    initList (state, data) {
-      state.goodList = data
-    },
-    addNum (state, obj) {
-      state.num = obj
-    },
-    add2Car (state, obj) {
-      console.log(obj)
-      if (state.goodList[obj.id] !== undefined) {
-        // 商品存在
-        state.goodList[obj.id] += obj.num
+    add(state, obj) {
+      const index = state.arr.findIndex(item => item.id === obj.id)
+      if (index !== -1) {
+        state.arr[index].num += obj.num
       } else {
-        // 商品不存在
-        // state.goodList[obj.id] = obj.num
-        // 如果是动态增加的属性 必须使用Vue.set才可以跟踪数据改变
-        // 参数1:对象  参数2:属性名  参数3:属性值
-        Vue.set(state.goodList, obj.id, obj.num)
-        this.state.num = obj.num
+        state.arr.push(obj)
       }
-      console.log(state)
+      window.localStorage.setItem('arr', JSON.stringify(state.arr))
+      // 次数
+      if (state.arr.length) {
+        let num = 0
+        state.arr.forEach(item => {
+          num += item.num
+        })
+        state.count = num
+      }
+    },
+    get(state) {
+      const arr = window.localStorage.getItem('arr')
+      state.arr = JSON.parse(arr) || []
+      console.log(state.arr)
+      // 次数
+      if (state.arr.length) {
+        let num = 0
+        state.arr.forEach(item => {
+          num += item.num
+        })
+        state.count = num
+      }
     }
-
   },
-  actions: {
-    // 获取页面数据
-    async getshopcarlist (context) {
-      const { data: res } = await axios.get('/api/goods/getshopcarlist/87,88,89')
-      console.log(res)
-      context.commit('initList', res.message)
-    }
-  },
-  modules: {
-
-  },
-  getters: {
-    money (state) {
-      let money = 0
-      state.goodList.forEach(item => {
-        money += item.sell_price * state.num
-      })
-      console.log(money)
-      return money
-    }
-  }
+  actions: {},
+  modules: {}
 })
